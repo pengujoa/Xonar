7012ac45b651f0e1d71daef33404ff0a643a282e
신창용 <ckddyd0806@gmail.com>
2022-01-18 20:14:58 +0900

    Update parse_pbtxt.py
    - Delete main
    - Update import_pbtxt function (to import subgraph pbtxt files)

--- parsing/parse_pbtxt.py
+++ parsing/parse_pbtxt.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from google.protobuf import text_format
+import os
 
 HOUR = 3600
 MINUTE = 60
@@ -8,14 +9,30 @@ ITERATIONS = 100
 
 # w1, w2에서 생성한 new pbtxt -> 결국 동일함.
 
-def import_pbtxt(file):
+
+def import_pbtxt(pbtxt_dir, w_idx):
+    file_list = os.listdir(pbtxt_dir)
+    pbtxt_list = [file for file in file_list if (file.startswith("_", 1) & ("w" + str(w_idx) in file))]
+
+    pbtxt_node = []
+
+    for file in pbtxt_list:
+        with open(pbtxt_dir + file) as f:
+            pbtxt = f.read()
+            graph_def = text_format.Parse(pbtxt, tf.compat.v1.GraphDef(), allow_field_number=1)
+            pbtxt_node = pbtxt_node + list(graph_def.node)
+
+    return pbtxt_node
+
+
+def import_pbtxt_old(file):
     with open(file) as f:
         pbtxt = f.read()
         graph_def = text_format.Parse(pbtxt, tf.compat.v1.GraphDef(), allow_field_number=1)
         return graph_def
 
 
-def classify_node(node_list, n_w, n_ps):
+def classify_node(node_list, n_ps, n_w):
     # ps는 CPU만 사용
     ps_cpu_send_rpc = [{} for _ in range(n_ps)]
     ps_cpu_recv_rpc = [{} for _ in range(n_ps)]
@@ -71,57 +88,32 @@ def classify_node(node_list, n_w, n_ps):
             elif recver_job == "worker":
                 if sender_job != recver_job or sender_task_idx != recver_task_idx:
                     if recver_device == "CPU":
-                        # w_cpu_recv_rpc[recver_task_idx].append(str(node.attr["tensor_name"]))
                         w_cpu_recv_rpc[recver_task_idx][str(node.attr["tensor_name"]).split("\"")[1]] = node.name
                     elif recver_device == "GPU":
                         w_gpu_recv_rpc[recver_task_idx][str(node.attr["tensor_name"]).split("\"")[1]] = node.name
-                        # w_gpu_recv_rpc[recver_task_idx].append(node.name)
                 else: # worker cpu -> worker gpu 또는 worker gpu -> worker cpu
                     # print("w->w인데, task까지 동일한 경우 존재. 즉 tensor 전송에 rpc 사용 안함.")
                     # print(node.name)
                     pass
 
-    print(len(ps_cpu_send_rpc[0]))
-    print(len(ps_cpu_send_rpc[1]))
-    print(len(w_cpu_send_rpc[0]))
-    print(len(w_cpu_send_rpc[1]))
-    print(len(w_gpu_send_rpc[0]))
-    print(len(w_gpu_send_rpc[1]))
+    # print(len(ps_cpu_send_rpc[0]))
+    # print(len(ps_cpu_send_rpc[1]))
+    # print(len(w_cpu_send_rpc[0]))
+    # print(len(w_cpu_send_rpc[1]))
+    # print(len(w_gpu_send_rpc[0]))
+    # print(len(w_gpu_send_rpc[1]))
+    #
+    # print(len(ps_cpu_recv_rpc[0]))
+    # print(len(ps_cpu_recv_rpc[1]))
+    # print(len(w_cpu_recv_rpc[0]))
+    # print(len(w_cpu_recv_rpc[1]))
+    # print(len(w_gpu_recv_rpc[0]))
+    # print(len(w_gpu_recv_rpc[1]))
 
-    print(len(ps_cpu_recv_rpc[0]))
-    print(len(ps_cpu_recv_rpc[1]))
-    print(len(w_cpu_recv_rpc[0]))
-    print(len(w_cpu_recv_rpc[1]))
-    print(len(w_gpu_recv_rpc[0]))
-    print(len(w_gpu_recv_rpc[1]))
-
-    return ps_cpu_send_rpc, w_cpu_send_rpc, w_gpu_send_rpc, ps_cpu_recv_rpc, w_cpu_recv_rpc, w_gpu_recv_rpc
+    return ps_cpu_send_rpc, ps_cpu_recv_rpc, w_cpu_send_rpc, w_cpu_recv_rpc, w_gpu_send_rpc, w_gpu_recv_rpc
 
 
-if __name__ == '__main__':
-    pbtxt = import_pbtxt("bench_log/new_imagenet_resnet101_v2_sync_batch64_w1.pbtxt")
-    ps_cpu_send_rpc_1, w_cpu_send_rpc_1, w_gpu_send_rpc_1, ps_cpu_recv_rpc_1, w_cpu_recv_rpc_1, w_gpu_recv_rpc_1 = classify_node(pbtxt.node, 2, 2)
-    print()
-    pbtxt = import_pbtxt("bench_log/new_imagenet_resnet101_v2_sync_batch64_w2.pbtxt")
-    ps_cpu_send_rpc_2, w_cpu_send_rpc_2, w_gpu_send_rpc_2, ps_cpu_recv_rpc_2, w_cpu_recv_rpc_2, w_gpu_recv_rpc_2 = classify_node(pbtxt.node, 2, 2)
-    # print("--test--")
-    # test = ps_cpu_send_rpc_1[0] + ps_cpu_send_rpc_2[0]
-    # print("before: ", len(test))
-    # print("after: ", len(list(set(test))))
-    # print("--test--")
-    # test = ps_cpu_send_rpc_1[1] + ps_cpu_send_rpc_2[1]
-    # print("before: ", len(test))
-    # print("after: ", len(list(set(test))))
-    # print("--test--")
-    # test = ps_cpu_recv_rpc_1[0] + ps_cpu_recv_rpc_2[0]
-    # print("before: ", len(test))
-    # print("after: ", len(list(set(test))))
-    # print("--test--")
-    # test = ps_cpu_recv_rpc_1[1] + ps_cpu_recv_rpc_2[1]
-    # print("before: ", len(test))
-    # print("after: ", len(list(set(test))))
-    # print("-----")
-    print(ps_cpu_recv_rpc_1[0])
+
 
 
 
